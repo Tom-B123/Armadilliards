95a641b7d44bd473d0bd907cc3361404d6a1a548
Tom-B123 <tom.h.barton@gmail.com>
2023-09-25 12:08:47 +0100

    manually connecting client to a hosted server to troublefix

--- Lobby Client/main.lua
+++ Lobby Client/main.lua
@@ -1,6 +1,6 @@
 local socket = require("socket")
 --The client of players, that will be connected to lobbies.
-local client = assert(socket.connect("localhost",500))
+local client = assert(socket.connect("localhost",1001))
 local server = nil
 --The identifier of the client
 local clientID = nil
