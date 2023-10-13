dd5745f34a4369ca7fba00cf8c3c01bfd7504bd0
Tom-B123 <tom.h.barton@gmail.com>
2023-10-13 19:30:52 +0100

    added verlet integration

--- Ball Rolling/main.lua
+++ Ball Rolling/main.lua
@@ -51,6 +51,8 @@ function Ball:new(x,y)
     object.y = y
     object.vx = 0
     object.vy = 0
+    object.ax = 0
+    object.ay = 0
     object.lx = x
     object.ly = y
     object.lvx = 0
@@ -69,17 +71,44 @@ function Ball:draw()
     love.graphics.print("pitch: "..self.pitch.."\nyaw: "..self.yaw)
 end
 
-function Ball:verlet()
+function Ball:verlet(dt)
+    local nextVX = (self.ax * dt * dt) + self.x - self.lx
+    local nextVY = (self.ay * dt * dt) + self.y - self.ly
+
+    self.vx = (self.vx + self.lvx) / 2
+    self.vy = (self.vy + self.lvy) / 2
+
+    local nextX = (self.x + self.vx)
+    local nextY = (self.y + self.vy)
+
+    self.lx = self.x
+    self.ly = self.y
+
+    self.x = nextX
+    self.y = nextY
+
+    self.lvx = self.vx * 0.99
+    self.lvy = self.vy * 0.99
+
+    self.vx = nextVX
+    self.vy = nextVY
+
+    self.ax = 0
+    self.ay = 0
+
+    if math.abs(self.vx) < 0.02 then self.vx = 0 end
+    if math.abs(self.vy) < 0.02 then self.vy = 0 end
 end
 
 local ball = Ball:new(100,100)
 
+ball.vx = 5
 
-
-function love.update()
-    local v = 1
-    ball.x = ball.x + v
-    ball.pitch = ball.pitch + calcAngle(v)
+function love.update(dt)
+    ball:verlet(dt)
+    -- local v = 1
+    -- ball.x = ball.x + v
+    -- ball.pitch = ball.pitch + calcAngle(v)
 end
 
 
