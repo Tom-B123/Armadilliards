967fc151948b2e58485a38ae2cf4c3727a261284
Tom-B123 <tom.h.barton@gmail.com>
2023-10-22 12:56:58 +0100

    shape:draw can draw polygons and circles

--- Complex Collisions/main.lua
+++ Complex Collisions/main.lua
@@ -6,10 +6,13 @@ function Shape:new(shape,dims,colour,fill)
     setmetatable(object,Shape)
     object.shape = shape
     object.colour = colour
-    object.fill = "line" and not fill or "fill" and fill
+    object.fill = "line"
+    if fill then object.fill = "fill" end
     if shape == "circle" then
-        object.x = dims[1]
-        object.y = dims[2]
+        object.coords = {
+            dims[1],
+            dims[2]
+        }
         object.radius = dims[3]
     elseif shape == "poly" then
         object.coords = dims
@@ -18,13 +21,25 @@ function Shape:new(shape,dims,colour,fill)
 end
 
 function Shape:draw()
-    love.graphics.circle(self.fill,self.x,self.y,self.radius)
+    if self.shape == "circle" then
+        love.graphics.circle(self.fill,self.coords[1],self.coords[2],self.radius)
+    elseif self.shape == "poly" then
+        love.graphics.polygon(self.fill,self.coords)
+    end
 end
 
-local circle = Shape:new()
+function Shape:move(x,y)
+    
+end
+
+local circle = Shape:new("circle",{100,100,50},{1,1,1},false)
+local triangle = Shape:new("poly",{200,200,300,200,250,300},{1,1,1},false)
 
 function love.update()
+    circle.x = circle.x + 1
 end
 
 function love.draw()
+    circle:draw()
+    triangle:draw()
 end
