105793c41655f549e1fccb1f03b6b26ad3492f90
Tom-B123 <tom.h.barton@gmail.com>
2023-10-22 13:26:28 +0100

    getLine function returns gradient,yInt

--- Complex Collisions/main.lua
+++ Complex Collisions/main.lua
@@ -37,10 +37,22 @@ function Shape:move(x,y)
 end
 
 function Shape:getLines()
+    local function isInf(num)
+        return num == math.huge or num == -math.huge
+    end
     local lx,ly = nil,nil
     local dx,dy = nil,nil
     local lines = {}
-    for i,coord in ipairs(self.coords) do
+    local corners = #self.coords / 2
+    --If it is a point or a line, loop one less time
+    if corners < 3 then
+        corners = corners - 1
+    end
+    for i = 1,(corners + 1) * 2 do
+        local coord = self.coords[i]
+        if i > #self.coords then 
+            coord = self.coords[i-#self.coords]
+        end
         if i % 2 == 1 then
             local x = coord
             if lx ~= nil then
@@ -52,7 +64,15 @@ function Shape:getLines()
             if ly ~= nil then
                 dy = ly - y
             end
-            table.insert(lines,(dy/dx))
+            if dx and dy then
+                local gradient = dy/dx
+                local yInt = "none"
+                if not isInf(gradient) then
+                    yInt = y - gradient * lx
+                end
+                local line = {gradient,yInt}
+                table.insert(lines,line)
+            end
             ly = y
         end
     end
@@ -60,14 +80,17 @@ function Shape:getLines()
 end
 
 local circle = Shape:new("circle",{100,100,50},{1,1,1},false)
-local triangle = Shape:new("poly",{200,200,300,200,250,300},{1,1,1},false)
+local square = Shape:new("poly",{300,380,400,310,440,410,340,410},{1,1,1},false)
 
 function love.update()
     circle:move(1,0)
-    triangle:move(2,0)
+    square:move(2,0)
 end
 
 function love.draw()
     circle:draw()
-    triangle:draw()
+    square:draw()
+    for i,line in ipairs(square:getLines()) do
+        love.graphics.print(line[1]..line[2],10,i*20)
+    end
 end
