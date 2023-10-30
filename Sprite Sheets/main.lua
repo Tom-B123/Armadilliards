27e9a5386dcf01a5dbea96b455a0f0b36c4fe4da
Tom-B123 <tom.h.barton@gmail.com>
2023-10-30 14:32:43 +0000

    drawing using spritesheet

--- Sprite Sheets/main.lua
+++ Sprite Sheets/main.lua
@@ -1,5 +1,20 @@
+local ballImages = {}
+
+
+
+local spritesheet = love.graphics.newImage("balls sheet.png")
+
+for i = 1,32 do
+    local quad = love.graphics.newQuad((i-1)*32,0,32,32,spritesheet)
+    ballImages[#ballImages+1] = quad
+end
+
+local tick = 0
+
 function love.update()
+    tick = tick + 1
 end
 
 function love.draw()
+    love.graphics.draw(spritesheet,ballImages[(tick%32)+1])
 end
