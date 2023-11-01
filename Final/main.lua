d52c1cb9c4b2baa8f6c6cce8c7349d8d75c58026
Tom-B123 <tom.h.barton@gmail.com>
2023-11-01 09:18:23 +0000

    tested lists

--- Final/main.lua
+++ Final/main.lua
@@ -4,9 +4,19 @@ require("switch")
 local switch = Switch:new()
 switch:addCase("data",function() love.graphics.print("data") end)
 
+local root = Node:new(10)
+local nRoot = root
+
+for i = 2,100 do
+    nRoot:add(root,i*10)
+    nRoot = nRoot.next
+end
+
 function love.update()
 end
 
 function love.draw()
     switch:case("data")
+    love.graphics.print(root.val,0,root.val/10)
+    root = root.prev
 end
