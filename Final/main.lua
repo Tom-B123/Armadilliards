7a7c92f2c383b6bbd89207c5daf1362d504cb46d
Tom-B123 <tom.h.barton@gmail.com>
2023-11-16 15:36:58 +0000

    made Lobby Player table

--- Final/main.lua
+++ Final/main.lua
@@ -9,14 +9,44 @@ local canQuit = false
 local tick = 0
 
 --record all player ids in the lobby
-local LobbyPlayer = {
-    playerIDs = {},
-    playerNameDict = {},
-    playerReadyDict = {},
-    playerTeamDict = {}
+LobbyPlayer = {
+    IDTable = {},
+    NameDict = {},
+    ReadyDict = {},
+    TeamDict = {}
 }
 function LobbyPlayer:new(id)
-    
+    table.insert(self.IDTable,id)
+end
+
+function LobbyPlayer:getName(id)
+    --returns the player name, or nil
+    local name = self.NameDict[id]
+    return name
+end
+
+function LobbyPlayer:setName(id,name)
+   self.NameDict[id] = name
+end
+
+function LobbyPlayer:getTeam(id)
+    --returns the player name, or nil
+    local team = self.TeamDict[id]
+    return team
+end
+
+function LobbyPlayer:setTeam(id,team)
+    self.TeamDict[id] = team
+end
+
+function LobbyPlayer:getReady(id)
+    --returns the player name, or nil
+    local isReady = self.ReadyDict[id]
+    return isReady
+end
+
+function LobbyPlayer:setReady(id,ready)
+    self.ReadyDict[id] = ready
 end
 
 local messageLog = {}
