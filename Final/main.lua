2f60866fa3e2e1d02f045c2705683930f6346d3d
Tom-B123 <tom.h.barton@gmail.com>
2023-11-21 09:41:27 +0000

    hypothesis that server is only processing data from one client

--- Final/main.lua
+++ Final/main.lua
@@ -137,6 +137,7 @@ end
 
 local function processReceived()
     local function process(data)
+        print(#data)
         for i,message in ipairs(data) do
             local splitData = split(message,":")
             local key,args = splitData[1],splitData[2]
