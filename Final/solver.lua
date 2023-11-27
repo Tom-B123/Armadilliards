936a0916d9bec158702b8a48f9caa1022dce2cc4
Tom-B123 <tom.h.barton@gmail.com>
2023-11-27 14:01:51 +0000

    added attributes for Ball class

--- Final/solver.lua
+++ Final/solver.lua
@@ -2,10 +2,18 @@ require("util")
 
 Ball = {}
 Ball.__index = Ball
-function Ball:new()
+function Ball:new(x,y)
     local object = {}
     setmetatable(object,Ball)
     object.ID = Util:calculateID(6)
+    object.x = x
+    object.y = y
+    object.vx = 0
+    object.vy = 0
+    object.lx = x
+    object.ly = y
+    object.lvx = 0
+    object.lvy = 0
 end
 
 Solver = {}
