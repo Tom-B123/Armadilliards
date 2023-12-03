8339a9c5b50b0476a194f210d4fb97f2fe35f3f6
Tom-B123 <tom.h.barton@gmail.com>
2023-12-03 10:40:30 +0000

    tested with dummy ID data, server able to handle multiple clients simultaniously

--- JS/main.lua
+++ JS/main.lua
@@ -1,5 +1,19 @@
 Socket = require("socket")
 
+--Returns a psudo-random string of numbers, with set length
+local function calculateID(length,salt)
+    local seed = Socket.gettime() * 10000
+    if salt then seed = seed + salt end
+    math.randomseed(seed)
+    local out = tostring(math.random(0,10^length - 1))
+    while #out < length do
+        out = "0"..out
+    end
+    return out
+end
+
+local ID = calculateID(6)
+
 local mainIP = "127.0.0.1"
 local mainPort = 500
 
@@ -8,7 +22,7 @@ local client = assert(Socket.connect(mainIP,mainPort))
 local msg = ""
 
 function love.update()
-    client:send("hello!")
+    client:send(ID)
     local data,err = client:receive()
     if data then msg = data end
 end
