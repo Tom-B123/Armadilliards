a1b0b59f37e6fa46480e7962cab721b75362d900
Tom-B123 <tom.h.barton@gmail.com>
2023-12-03 11:08:48 +0000

    testing switch block for processing net commands, working

--- JS/mainServer.js
+++ JS/mainServer.js
@@ -17,9 +17,42 @@ function split(string, sep) {
     return out;
 }
 
-const splitData = split("updt:ID_name_player 3",":");
-console.log(splitData[0],split(splitData[1],"_"));
+function netSwitch(message) {
+    const splitCommand = split(message,":");
+    const command = splitCommand[0];
+    const args    = splitCommand[1];
+    const splitData = split(args,"_")
+    // Net switch, converts the command into a function to execute
+    // Commands that will be received: ncon, econ, updt?, create, join
+    switch (command) {
+        case "ncon":
+            var playerID = splitData[0];
+            console.log("new connection: " + playerID);
+            break;
+        case "econ":
+            var playerID = splitData[0];
+            console.log("end connection: " + playerID);
+            break;
+        case "create":
+            var playerID = splitData[0];
+            console.log("create new lobby: " + playerID);
+            break;
+        case "join":
+            var playerID = splitData[0];
+            var lobbyID = splitData[1];
+            console.log("player: " + playerID + " to join lobby: " + lobbyID);
+            break;
+        default:
+            console.log("unknown command")
 
+    }
+}
+
+netSwitch("ncon:123981234_124234")
+netSwitch("econ:123981234_124234")
+netSwitch("create:123981234_124234")
+netSwitch("join:123981234_124234")
+netSwitch("amingus:123981234_124234")
 const server = net.createServer((socket) => {
     console.log('Client connected.');
 
